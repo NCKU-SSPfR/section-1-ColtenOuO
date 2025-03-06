e86eae6f7ac4a1ef1dde192f3e25e46b1e685538
ColtenOuO <jun930436@gmail.com>
2025-03-06 16:26:24 +0800

    fixed: calculate.py estimated_pi variable named

--- CleanCodeHW/calculate_pi.py
+++ CleanCodeHW/calculate_pi.py
@@ -17,6 +17,6 @@ for _ in range(NUM_POINTS):
         inside_circle += POINT_COUNT_INCREMENT
 
 # Estimate pi based on the number of points inside the circle
-pi_neapple = (inside_circle / NUM_POINTS) * AREA_FACTOR
+estimated_pi = (inside_circle / NUM_POINTS) * AREA_FACTOR
 
-print(f"Estimated value of pi is: {pi_neapple}")
+print(f"Estimated value of pi is: {estimated_pi}")
