d0b52e6c99ee5c667b322f5c83801597fab5c174
ColtenOuO <jun930436@gmail.com>
2025-03-10 13:50:43 +0800

    refactor: rename variable

--- CleanCodeHW/calculate_pi.py
+++ CleanCodeHW/calculate_pi.py
@@ -7,16 +7,16 @@ AREA_FACTOR = 4
 SQUARE_EXPONENT = 2
 POINT_COUNT_INCREMENT = 1
 
-point_inside_circle = 0
+inside_circle = 0
 
 # Randomly generate points and count those inside the circle
-for _ in range(NUM_POINTS):
+for point in range(NUM_POINTS):
     x = random.uniform(-RADIUS, RADIUS)
-    y = random.uniform(-1, 1)
+    y = random.uniform(-RADIUS, RADIUS)
     if x**SQUARE_EXPONENT + y**SQUARE_EXPONENT <= RADIUS**SQUARE_EXPONENT:
-        point_inside_circle  += POINT_COUNT_INCREMENT
+        inside_circle += POINT_COUNT_INCREMENT
 
 # Estimate pi based on the number of points inside the circle
-pi_estimated = (point_inside_circle  / NUM_POINTS) * AREA_FACTOR
+pi = (inside_circle / NUM_POINTS) * AREA_FACTOR
 
-print(f"Estimated value of pi is: {pi_estimated}")
+print(f"Estimated value of pi is: {pi}")
